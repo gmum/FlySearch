5ea3d4ccbd109305aaa9d4f6a148c71e9dd6d63a
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-09-16 20:41:26 +0200

    hr bench

---
+++ hr_bench.py
@@ -0,0 +1,50 @@
+import base64
+import torch
+import random
+
+from datasets import load_dataset
+from PIL import Image
+from io import BytesIO
+
+ds = load_dataset("DreamMr/HR-Bench")
+
+
+class HRBench(torch.utils.data.Dataset):
+    def __init__(self):
+        self.data = ds["train"]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        raw_image = item["image"]
+        pil_image = Image.open(BytesIO(base64.b64decode(raw_image)))
+
+        answer = item["answer"]
+        question = item["question"]
+
+        options = [item["A"], item["B"], item["C"], item["D"]]
+        random.shuffle(options)
+
+        return pil_image, question, options, answer
+
+
+def main():
+    ds = HRBench()
+
+    for i in range(10):
+        image, question, options, answer = ds[i]
+
+        print(question)
+        print(options)
+        print(answer)
+
+        image.show()
+
+        break
+
+
+if __name__ == "__main__":
+    main()
