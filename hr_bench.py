9fbe0c2377a23fb1f536718be4e575ac1a33bc4f
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-09-17 13:45:27 +0200

    readjusted code not to use huggingface in hrbench

--- hr_bench.py
+++ hr_bench.py
@@ -1,24 +1,22 @@
 import base64
 import torch
 import random
+import pandas as pd
 
-from datasets import load_dataset
 from PIL import Image
 from io import BytesIO
-
-ds = load_dataset("DreamMr/HR-Bench")
 
 
 class HRBench(torch.utils.data.Dataset):
     def __init__(self, transform=None):
-        self.data = ds["train"]
+        self.data = pd.read_parquet("data/hr_bench_8k.parquet")
         self.transform = transform
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        item = self.data[idx]
+        item = self.data.iloc[idx]
 
         raw_image = item["image"]
         image = Image.open(BytesIO(base64.b64decode(raw_image)))
@@ -37,20 +35,14 @@ class HRBench(torch.utils.data.Dataset):
 
 
 def main():
-    ds = HRBench()
+    dataset = HRBench()
+    print(f"Number of samples: {len(dataset)}")
 
-    print(len(ds))
-
-    for i in range(10):
-        image, question, options, answer = ds[i]
-
-        print(question)
-        print(options)
-        print(answer)
-
-        image.show()
-
-        break
+    image, question, options, answer = dataset[0]
+    print(f"Image: {image}")
+    print(f"Question: {question}")
+    print(f"Options: {options}")
+    print(f"Answer: {answer}")
 
 
 if __name__ == "__main__":
